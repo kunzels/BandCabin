56acf033c789ae1501f817d688a9efdc6b1435cd
kunzels <kunzelsteven@gmail.com>
2020-04-06 16:58:28 -0400

    Heroku Prep Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,11 @@
 class User < ApplicationRecord
     attr_reader :password
 
-    validates :username, :email :password_digest, :session_token, presence: true
+    validates :username, :email, :password_digest, :session_token, presence: true
     validates :username, :email, uniqueness: true
     validates :password, length: {minimum: 6}, allow_nil: true
+
+    after_initialize :ensure_session_token
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
