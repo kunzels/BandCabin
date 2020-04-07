52cf2029c0b6915fc087759ac3d389e4796e2ffc
kunzels <kunzelsteven@gmail.com>
2020-04-07 09:39:30 -0400

    Prep for nav component

--- frontend/BandCabin.jsx
+++ frontend/BandCabin.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {signup, login, logout} from "./util/session_api_util";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    const store = configureStore();
+
+    window.login = login;
+    window.signup = signup;
+    window.logout = logout;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to BandCabin</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
