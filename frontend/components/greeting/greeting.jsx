d52e000d42dd04f27e7ccc1d07cdbabad53b3be6
kunzels <kunzelsteven@gmail.com>
2020-04-08 01:03:38 -0400

    Some styling added to nav and form

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -3,17 +3,30 @@ import { Link } from 'react-router-dom'
 
 const Greeting = ({currentUser, logout}) => {
     const personalGreeting = () => (
-        <div className = "test">
-            <h1>Welcome to BandCabin, {currentUser.username}</h1>
+        <div className = "nav-bar">
+            <Link to="/" >
+                <img className ="logo-link" src={window.logo} alt="" />
+            </Link>
+            <h1 className="welcome">{currentUser.username}</h1>
+            <div className="logout-button">
             <button onClick={logout}>Log Out</button>
+            </div>
         </div>
     );
 
     const sessionLinks = () => (
-        <div>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up!</Link>
-            
+        <div className="nav-bar"> 
+            <div className="left-side-nav">
+            <Link to="/" >
+                <img className="logo-link" src={window.logo} alt="" />
+            </Link>
+            <p>Discover amazing new music and directly support the artists who make it.</p>
+           </div>
+
+            <div className="right-side-nav">
+                <Link to="/signup" className="login-links-left">sign up   </Link>
+                <Link to="/login" className="login-links-right">log in</Link>
+            </div>
         </div>
     );
 
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -37,21 +37,28 @@ class SessionForm extends React.Component {
 
     render() {
         return (
-            <div>
-                <form className ="formContainer" onSubmit={this.handleSubmit}>
-                    Welcome to Bandcabin!
-                        
+            <div className="form-container" >
+                <form className="form-container" onSubmit={this.handleSubmit}>
+                    
+                    <h3>Welcome to Bandcabin</h3>  
+                    <p className="divider">-------------------------
+                    ------------------------------------------------
+                    ------------------------------------------------
+                    ----------------------</p>
                     {this.renderErrors()}
-                    <div>
+                    <div className = "login-input">
                         <label>Username:
                             <input type="text" value={this.state.username} onChange={this.update('username')}/>
                         </label>
-                        <label>Email:
+                    <br/>
+                        <label className="email-input">Email:
                             <input type="text" value={this.state.email} onChange={this.update('email')} />
                         </label>
+                    <br/>
                         <label>Password:
                             <input type="password" value={this.state.password} onChange={this.update('password')} />
                         </label>
+                    <br/>
                         <input type="submit" value={this.props.formType} />
                     </div>
                 </form>
